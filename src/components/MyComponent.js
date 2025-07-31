61ecdcff85729d44fcb7069bb05f2e7885e2765d
namle10102003 <Namle10102003@gmail.com>
2025-07-31 09:27:55 +0700

    Learned State

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -1,12 +1,16 @@
 import React from "react";
 
 class MyComponent extends React.Component {
+    state = {
+        name: 'Nam Le',
+        address: 'Da Nang',
+        age: 22
+    };
+
     render() {
         return (
             <div>
-                My First Component
-
-                {Math.random()};
+                My name is {this.state.name} and I'm from {this.state.address}
             </div>
         );
     };
